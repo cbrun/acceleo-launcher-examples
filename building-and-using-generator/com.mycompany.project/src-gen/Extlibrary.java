9f31f66ae8c79690821b505bf58e3b781695bcf9
cbrun <cedric.brun@obeo.fr>
2016-01-27 15:29:24 +0100

    Add another example with a generator which is being built as it's being used

---
+++ building-and-using-generator/com.mycompany.project/src-gen/Extlibrary.java
@@ -0,0 +1,46 @@
+public class Extlibrary {
+
+  class Book {
+  }
+
+  class Library {
+  }
+
+  class Writer {
+  }
+
+  class Item {
+  }
+
+  class Lendable {
+  }
+
+  class CirculatingItem {
+  }
+
+  class Periodical {
+  }
+
+  class AudioVisualItem {
+  }
+
+  class BookOnTape {
+  }
+
+  class VideoCassette {
+  }
+
+  class Borrower {
+  }
+
+  class Person {
+  }
+
+  class Employee {
+  }
+
+  class Addressable {
+  }
+
+
+}
